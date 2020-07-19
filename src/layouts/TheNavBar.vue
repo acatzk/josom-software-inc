1c25e0b38b77eddd29cbf98060f05b6bd7a99d72
acatzk <joshuaimalay@gmail.com>
2020-07-19 09:04:26 +0800

    Added v-app-bar with menu, icon and options button

---
+++ src/layouts/TheNavBar.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <v-app-bar
+        absolute
+        color="white"
+        elevate-on-scroll
+    >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-title>
+            <v-icon>mdi-twitter</v-icon>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+
+    </v-app-bar>
+
+</template>
