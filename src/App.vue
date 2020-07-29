2cf7c348e09b25ebebf347a69a94fd0a9a98962e
acatzk <joshuaimalay@gmail.com>
2020-07-30 07:35:00 +0800

    Linked the app.scss file style

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,4 @@
 
 </template>
 
-<style scoped lang="scss">
-
-@import url('https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap');
-
-.v-application {
-  font-family: "Poppins", sans-serif !important;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-body, html {
-  font-family: "Poppins", sans-serif !important;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-</style>
+<style scoped lang="scss" src="@/assets/scss/app.scss"></style>
