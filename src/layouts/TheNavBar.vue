66cd23aedcb9f893f791e70bdd1b5519cffbb478
acatzk <joshuaimalay@gmail.com>
2020-07-30 08:51:15 +0800

    Changed business icon

--- src/layouts/TheNavBar.vue
+++ src/layouts/TheNavBar.vue
@@ -5,18 +5,20 @@
         color="white"
         elevate-on-scroll
     >
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-btn icon>
+            <v-icon class="iconify" data-icon="heroicons-outline:menu-alt-4"></v-icon>
+        </v-btn>
 
         <v-spacer></v-spacer>
 
-        <v-toolbar-title class="d-flex">
-            <v-icon>mdi-instagram</v-icon> Josom
+        <v-toolbar-title class="d-flex" icon>
+            <v-icon class="iconify" data-icon="mdi-cube-send"></v-icon> Josom
         </v-toolbar-title>
 
         <v-spacer></v-spacer>
 
         <v-btn icon>
-            <v-icon>mdi-dots-vertical</v-icon>
+            <v-icon class="iconify" data-icon="entypo:dots-two-vertical"></v-icon>
         </v-btn>
 
     </v-app-bar>
