27401af253a54f02283f3360f360e379444b5538
acatzk <joshuaimalay@gmail.com>
2020-08-01 08:26:18 +0800

    Added function for smooth scroll link

---
+++ src/components/mixins/ScrollLink.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <a :href="href" @click.prevent="scroll">
+
+        <slot></slot>
+        
+    </a>
+
+</template>
+
+<script>
+
+export default {
+
+    name: 'ScrollLink',
+
+    props: ['href'],
+
+    methods: {
+
+        scroll() {
+
+            document.querySelector(this.href)
+                    .scrollIntoView({ behavior: 'smooth' })
+        }
+
+    }
+
+}
+
+</script>
