d231d4c30c8623d9f43a66ded104aecd80291882
acatzk <joshuaimalay@gmail.com>
2020-08-01 09:08:18 +0800

    Import the VueFbCustomerChat component

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from '@/router'
 import store from  '@/store'
 import vuetify from '@/plugins/vuetify'
+import VueFbCustomerChat from 'vue-fb-customer-chat'
+
+Vue.use(VueFbCustomerChat, {
+  page_id: '106304557778712', //  change 'null' to your Facebook Page ID,
+  theme_color: '#333333', // theme color in HEX
+  locale: 'en_US', // default 'en_US'
+})
 
 Vue.config.productionTip = false
 
