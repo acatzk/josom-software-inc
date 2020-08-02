3f108108b78235da71bac385405ddae612d02808
acatzk <joshuaimalay@gmail.com>
2020-08-02 15:06:47 +0800

    Import Services vue component

---
+++ src/components/pages/Services.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-card
+        class="mx-auto"
+        flat
+        id="services"
+        :dark="mode ? false : true"
+    >
+        <v-container>
+            <h1>Services</h1>
+        </v-container>
+    </v-card>
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+    name: 'about',
+
+    computed: {
+        ...mapState(['mode'])
+    }
+}
+</script>
+
+<style scoped>
+.v-card {
+    border-radius: 0 !important;
+    -moz-webkit-border-radius: 0;
+    min-height: 100vh;
+    padding-top: 50px;
+}
+</style>
