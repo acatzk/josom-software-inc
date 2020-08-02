c7b711ca6168e4ac27ceb236b5c55e5e2d89cf64
acatzk <joshuaimalay@gmail.com>
2020-08-02 08:48:04 +0800

    Dispalyed dummy image with full height

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card
+    class="mx-auto"
+    flat
+  >
+    <v-img
+      class="white--text align-end"
+      height="90vh"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    >
+      <!-- <v-card-title>Top 10 Australian beaches</v-card-title> -->
+    </v-img>
+  </v-card>
+</template>
+
+<style scoped>
+.v-card {
+    border-radius: 0 !important;
+    -moz-webkit-border-radius: 0 !important;
+}
+
+</style>
