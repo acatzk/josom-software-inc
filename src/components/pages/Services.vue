c14be02c59e76935cf9e4cca845e2920a4795a51
acatzk <joshuaimalay@gmail.com>
2020-08-02 16:34:26 +0800

    Added v-card empty content

--- src/components/pages/Services.vue
+++ src/components/pages/Services.vue
@@ -1,12 +1,71 @@
 <template>
     <v-card
-        class="mx-auto"
+        class="mx-auto services"
         flat
         id="services"
+        :style="mode ? 'background-color: #f7f7f7;' : 'background-color: #363636;'"
         :dark="mode ? false : true"
     >
         <v-container>
-            <h1>Services</h1>
+
+            <div class="font-weight-normal text-uppercase subtitle text-center what mt-2">
+                WHAT WE DO
+            </div>
+            <v-card-text class="font-weight-bold text-uppercase title text-center our">
+                OUR SERVICES
+            </v-card-text>
+
+
+             <div class="elevation-0">
+                <v-row class="d-flex justify-space-between">
+
+                    <v-col cols="12" sm="4">
+                        <v-card
+                            :dark="mode ? false : true"
+                            style="min-height: 70vh;"
+                            hover
+                            flat
+                            router
+                            to="/"
+                            class="elevation-5"
+                        >
+                            <v-container>
+                               
+                            </v-container>
+                        </v-card>
+                    </v-col>
+
+                    <v-col cols="12" sm="4">
+                        <v-card
+                            :dark="mode ? false : true"
+                            style="min-height: 70vh;"
+                            hover
+                            flat
+                            class="elevation-5"
+                        >
+                            <v-container>
+                               
+                            </v-container>
+                        </v-card>
+                    </v-col>
+
+                    <v-col cols="12" sm="4">
+                        <v-card
+                            :dark="mode ? false : true"
+                            style="min-height: 70vh;"
+                            hover
+                            flat
+                            class="elevation-5"
+                        >
+                            <v-container>
+                               
+                            </v-container>
+                        </v-card>
+                    </v-col>
+                
+                </v-row>
+             </div>
+
         </v-container>
     </v-card>
 </template>
@@ -24,11 +83,12 @@ export default {
 }
 </script>
 
-<style scoped>
-.v-card {
+<style scoped lang="scss">
+.services {
     border-radius: 0 !important;
     -moz-webkit-border-radius: 0;
     min-height: 100vh;
     padding-top: 50px;
 }
+
 </style>
