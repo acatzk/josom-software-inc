95f32b9bcf7ee239d64745084102f54bb3a49730
acatzk <joshuaimalay@gmail.com>
2020-08-03 08:33:02 +0800

    Changed the arrow up icon

--- src/components/mixins/BackToTop.vue
+++ src/components/mixins/BackToTop.vue
@@ -1,26 +1,68 @@
 <template>
-  <div id="app">
+
+  <div>
+
     <v-flex xs10>
-        <v-btn v-scroll='onScroll' v-show="fab" fab dark fixed bottom right color="#FFC400" @click="toTop">
-          <v-icon>arrow_circle_up</v-icon>
+
+        <v-btn 
+            v-scroll='onScroll'
+            v-show="fab" 
+            fab 
+            dark 
+            fixed 
+            bottom 
+            right 
+            :color="mode ? 'primary' : 'error'" 
+            @click="toTop"
+        >
+
+          <v-icon>mdi-arrow-up</v-icon>
+
         </v-btn>
+
     </v-flex>
+
   </div>
+
 </template>
+
 <script>
-export default {
-  data: () => ({
-    fab:false
-  }),
-  methods:{
-    onScroll (e) {
-      if(typeof window === 'undefined') return
-      const top = window.pageYOffset || e.target.scrollTop || 0
-      this.fab = top > 10
+
+import { mapState } from 'vuex'
+
+export default {   
+
+    name: 'back-top-top',
+
+    data () {
+        return {
+            fab:false
+        }
     },
-    toTop (){
-      this.$vuetify.goTo(0)
+
+    computed: {
+        ...mapState(['mode'])
+    },
+
+    methods:{
+
+        onScroll (e) {
+
+        if(typeof window === 'undefined') return
+
+        const top = window.pageYOffset || e.target.scrollTop || 0
+
+        this.fab = top > 10
+
+        },
+
+        toTop (){
+            
+        this.$vuetify.goTo(0)
+
+        }
+
     }
-  }
+
 }
 </script>>
