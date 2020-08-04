e552001a8f475eb49329ae6018ed3e610b5c4384
acatzk <joshuaimalay@gmail.com>
2020-08-04 08:29:56 +0800

    Added vue-particles for better homepage background image

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -4,41 +4,57 @@
         flat
         id="home"
     >
-        <v-img
-            class="white--text"
-            height="100vh"
-            src="@/assets/imgs/home.jpg"
-            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.2)"
+        <v-container
+            class="fill-height"
+            fluid
+            style="position: absolute;"
         >
-            <v-container
-                class="fill-height"
-                fluid
-            >
-                <v-row>
-                    <v-col 
-                        cols="12" 
-                        align="center"
-                        justify="center"
+            <v-row>
+                <v-col 
+                    cols="12" 
+                    align="center"
+                    justify="center"
+                >
+                    <h1 class="text-uppercase white--text">
+                        Making great things possible
+                    </h1>
+                    <span class="white--text">
+                        Josom Software, Inc.
+                    </span> 
+                    <br>
+                    <v-btn
+                        depressed
+                        color="blue"
+                        class="white--text mt-5"
+                        x-large
+                        @click.prevent="scroll"
                     >
-                        <div class="title font-weight-bold text-uppercase">
-                            Making great things possible
-                        </div>
-                        <div class="subtitle mt-5 font-weight-light">
-                            Josom Software, Inc.
-                        </div>
-                        <v-btn
-                            depressed
-                            color="blue"
-                            class="white--text mt-5"
-                            x-large
-                            @click.prevent="scroll"
-                        >
-                            DISCOVER MORE <v-icon right>mdi-arrow-right</v-icon>
-                        </v-btn>
-                    </v-col>
-                </v-row>
-            </v-container>
-        </v-img>
+                        DISCOVER MORE <v-icon right>mdi-arrow-right</v-icon>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+
+        <vue-particles
+            color="#fff"
+            :particleOpacity="0.7"
+            linesColor="#fff"
+            :particlesNumber="80"
+            shapeType="circle"
+            :particleSize="5"
+            :linesWidth="2"
+            :lineLinked="true"
+            :lineOpacity="0.4"
+            :linesDistance="150"
+            :moveSpeed="3"
+            :hoverEffect="true"
+            hoverMode="grab"
+            :clickEffect="true"
+            clickMode="push"
+            style="height: 100vh;"
+            class="particles"
+        >
+        </vue-particles>
     </v-card>
 </template>
 
@@ -59,12 +75,21 @@ export default {
 .v-card {
     border-radius: 0 !important;
     -moz-webkit-border-radius: 0 !important;
+    text-align: center;
 
-    .title {
+    h1 {
         font-size: 38px !important;
     }
-    .subtitle {
+    span {
         font-size: 20px !important;
     }
+
 }
+
+.particles {
+    background-image: linear-gradient(rgba(80, 80, 80, 0.5), rgba(0, 0, 0, .5)), url("../../assets/imgs/home.jpg");
+    background-size: cover;
+
+}
+
 </style>
