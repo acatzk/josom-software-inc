e73978149a03e1c6943079a3c19f5928e730c1d2
acatzk <joshuaimalay@gmail.com>
2020-08-06 11:27:11 +0800

    Import CardCarousel vue component

---
+++ src/components/pages/Projects.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-card
+        class="mx-auto projects"
+        flat
+        id="projects"
+        :dark="mode ? false : true"
+    >
+        <v-container>
+
+            <v-flex class="text-center heading">
+                <div class="font-weight-normal text-uppercase subtitle">
+                    PORTFOLIO
+                </div>
+                <v-card-text class="font-weight-bold text-uppercase title">
+                    <h2>OUR PROJECTS</h2>
+                </v-card-text>
+            </v-flex>
+
+            <card-carousel />
+
+        </v-container>
+    </v-card>
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+    name: 'projects',
+
+    computed: {
+        ...mapState(['mode'])
+    },
+
+    components: {
+        CardCarousel: () => import('@/components/mixins/CardCarousel')
+    }
+}
+</script>
+
+<style scoped>
+.projects {
+    min-height: 100vh;
+    padding-top: 50px;
+}
+h2 {
+    font-size: 2.2rem;
+    font-family: 'Teko', sans-serif;
+    text-transform: uppercase;
+    margin: 0;
+}
+</style>
