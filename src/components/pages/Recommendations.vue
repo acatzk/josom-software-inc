650a5c5ea75fdef7594eba366de9d7e645374cdd
acatzk <joshuaimalay@gmail.com>
2020-08-07 12:02:53 +0800

    Import Carousel3d images

---
+++ src/components/pages/Recommendations.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-card id="recommendations" :dark="mode ? true : true">
+
+        <v-container>
+             <v-flex class="text-center heading">
+                <div class="font-weight-normal text-uppercase subtitle">
+                    MY TRUE HAPPINESS
+                </div>
+                <v-card-text class="font-weight-bold text-uppercase title">
+                    <h2>RECOMMENDATIONS</h2>
+                </v-card-text>
+            </v-flex>
+
+            <carousel3d />
+
+        </v-container>
+
+    </v-card>
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+    name: 'recommendations',
+
+    components: {
+        Carousel3d: () => import('@/components/mixins/Carousel3d')
+    },
+
+    computed: {
+        ...mapState(['mode'])
+    }
+}
+</script>
+
+<style scoped>
+.v-card {
+    border-radius: 0 !important;
+    -moz-webkit-border-radius: 0;
+    height: 100vh;
+    background-image: linear-gradient(rgba(2, 2, 2, 0.7), rgba(0, 0, 0, .6)), url('../../assets/imgs/docks.jpg');
+    background-size: cover;
+    padding-top: 50px;
+}
+h2 {
+    font-size: 2.2rem;
+    font-family: 'Teko', sans-serif;
+    text-transform: uppercase;
+    margin: 0;
+}
+</style>
