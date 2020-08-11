3de35be311f5f90a66eb41d483ef51ca988d2079
acatzk <joshuaimalay@gmail.com>
2020-08-11 12:41:21 +0800

    Displayed the client logo images

--- src/components/pages/OurClients.vue
+++ src/components/pages/OurClients.vue
@@ -15,6 +15,19 @@
                     <h2>OUR SATISFIED CLIENTS</h2>
                 </v-card-text>
             </v-flex>
+
+            <vue-horizontal-list :items="items" class="mx-4">
+                <template v-slot:default="{item}">
+                    <v-img
+                        height="200px"
+                        :src="item.image"
+                        class="white--text align-end"
+                        :lazy-src="item.image"
+                    >
+                    </v-img>
+                </template>
+            </vue-horizontal-list>
+
         </v-container>
     </v-card>
 </template>
@@ -26,8 +39,37 @@ import { mapState } from 'vuex'
 export default {
     name: 'our-clients',
 
+    data () {
+        return {
+            items: [
+                {
+                    image: 'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0011/8865/brand.gif?itok=NMrbptII'
+                },
+                {
+                    image: 'https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/042012/mang-inasal-logo1.jpg?itok=O_ztSdvT'
+                },
+                {
+                    image: 'https://i.ytimg.com/vi/394VjIrmgaY/maxresdefault.jpg'
+                },
+                {
+                    image: 'https://fontmeme.com/images/red-ribbon-logo.png'
+                },
+                {
+                    image: 'https://seeklogo.com/images/B/Burger_King-logo-67A54F414B-seeklogo.com.png'
+                },
+                {
+                    image: 'https://mallfoodonabudget.files.wordpress.com/2013/08/chow-king.png?w=584'
+                }
+            ]
+        }
+    },
+
     computed: {
         ...mapState(['mode'])
+    },
+
+    components: {
+        VueHorizontalList: () => import('vue-horizontal-list')
     }
     
 }
