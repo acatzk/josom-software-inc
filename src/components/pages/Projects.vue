7580813a9b26a8209a84dddc8cffa296354b05db
acatzk <joshuaimalay@gmail.com>
2020-08-15 11:12:24 +0800

    Import horizontallist and passed the slots item insdie it

--- src/components/pages/Projects.vue
+++ src/components/pages/Projects.vue
@@ -17,7 +17,42 @@
             </v-flex>
 
             <!-- <card-carousel /> -->
-            <horizontal-list />
+            <v-container>
+                <horizontal-list :items="items">
+                    <template v-slot:default="{ item }">
+                        <v-card
+                            class="mx-auto"
+                            max-width="500"
+                        >
+                            <v-img
+                                height="200px"
+                                :src="item.image"
+                                class="white--text align-end"
+                                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                            >
+                                <h4 class="ml-3">Year {{  item.projectYear}}</h4>
+                            </v-img>
+                            
+                            <v-card-title>{{ item.title }}</v-card-title>
+
+                            <v-card-subtitle class="text-justify">
+                                {{ item.content }}
+                            </v-card-subtitle>
+
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn 
+                                    depressed
+                                    small
+                                    text
+                                >
+                                <v-icon left>mdi-web</v-icon> Demo
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </template>
+                </horizontal-list>
+            </v-container>
 
         </v-container>
     </v-card>
@@ -27,16 +62,29 @@
 
 import { mapState } from 'vuex'
 
+import projects from '@/static/projects.json'
+
 export default {
     name: 'projects',
 
-    computed: {
-        ...mapState(['mode'])
+    data() {
+        return {
+            items: []
+        }
     },
 
     components: {
         CardCarousel: () => import('@/components/mixins/CardCarousel'),
         HorizontalList: () => import('@/components/mixins/HorizontalList')
+    },
+
+    computed: {
+        ...mapState(['mode'])
+    },
+
+    created () {
+        let projectData = projects.projects
+        return this.items.push(...projectData)
     }
 }
 </script>
