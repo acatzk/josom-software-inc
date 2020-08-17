f7f93d29c25e5512dc9bd8841757a1255a13825c
acatzk <joshuaimalay@gmail.com>
2020-08-17 10:47:03 +0800

    Added new button social links

--- src/components/layouts/TheFootBar.vue
+++ src/components/layouts/TheFootBar.vue
@@ -8,15 +8,22 @@
       flat
       style="background: transparent;"
     >
-      <v-card-text class="py-1 text-center">
+      <v-card-text class="py-2 text-center">
         <v-btn
-          v-for="icon in icons"
-          :key="icon"
+          v-for="(link, index) in links"
+          :key="index"
           class="mx-4"
           dark
           icon
+          :href="link.link"
+          target="_blank"
         >
-          <v-icon size="24px" color="grey">{{ icon }}</v-icon>
+          <v-icon 
+            size="24px" 
+            color="grey" 
+            :data-icon="link.icon === 'cib-stackoverflow' ? 'cib-stackoverflow' : undefined"
+            :class="link.icon === 'cib-stackoverflow' ? 'iconify' : undefined"
+          >{{ link.icon }}</v-icon>
         </v-btn>
       </v-card-text>
     </v-card>
@@ -27,11 +34,31 @@
 import { mapState } from 'vuex'
 export default {
   data: () => ({
-    icons: [
-      'mdi-facebook',
-      'mdi-twitter',
-      'mdi-linkedin',
-      'mdi-instagram',
+    links: [
+      {
+        icon: 'mdi-facebook',
+        link: 'https://facebook.com'
+      },
+      {
+        icon: 'mdi-twitter',
+        link: 'https://twitter.com'
+      },
+      {
+        icon: 'mdi-linkedin',
+        link: 'https://linkedin.com'
+      },
+      {
+        icon: 'mdi-instagram',
+        link: 'https://instagram.com'
+      },
+      {
+        icon: 'mdi-github',
+        link: 'https://github.com'
+      },
+      {
+        icon: 'cib-stackoverflow',
+        link: 'https://stackoverflow.com'
+      }
     ],
   }),
   computed: {
