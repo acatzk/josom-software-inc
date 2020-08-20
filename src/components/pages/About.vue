307e772fec509798c06882f1c85760f781442076
acatzk <joshuaimalay@gmail.com>
2020-08-20 11:48:39 +0800

    Removed the last part of Josom sentense.

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -16,7 +16,7 @@
                 Josom Sotware Incorporation is an IT solutions company in the Philippines that focus on catering very quality website / web application software and feels the UX as its finest. Goes way back 2019 - A young & aspiring IT graduated, dream to become software developer in the near future. But things are not just easy, he applied more than 20 job applications as a software developer and sooner he was hired. While he was working, his passion and eagerness to learn anything under any circumstances push him to surpass his limit on doing what he love most. He tried to get a sideline project while he was working to gain more experience. He was thinking that if he do a sideline projects while working, will makes his self development even more faster ( money was just a bonus for him ) .
                 <br />
                 <br />
-                As time goes by, Several clients trust him because of his credibility and dedication on his work. He always finished the project ahead of time. He never fail his clients. In year 2020 and also being a fullstack developer. He wants to make a change. He started to make a start up firm for business IT solutions not for the money but for his passion. He name the company " JOSOM " meaning Joshua with collaboration of Jerome in Short Josom Software, Inc.
+                As time goes by, Several clients trust him because of his credibility and dedication on his work. He always finished the project ahead of time. He never fail his clients. In year 2020 and also being a fullstack developer. He wants to make a change. He started to make a start up firm for business IT solutions not for the money but for his passion.
             </v-card-text>
         </v-container>
     </v-card>
